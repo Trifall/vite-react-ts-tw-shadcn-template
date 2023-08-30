07fd25640d802d86fb2aff27ffef873fbdc95fa1
Trifall <JerrenT@Trifall.com>
2023-08-30 06:16:02 -0400

    add dark mode theme

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
 import '@/styles/global.css';
+import { ThemeProvider } from '@/styles/theme-provider.tsx';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
 	<React.StrictMode>
-		<App />
+		<ThemeProvider defaultTheme='dark' storageKey='vite-ui-theme'>
+			<App />
+		</ThemeProvider>
 	</React.StrictMode>
 );
+
+export default App;
