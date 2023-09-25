7ae1de81f84c68579918bcb1bae9683d7ea8cdda
Trifall <JerrenT@Trifall.com>
2023-09-25 16:29:36 -0400

    fix lint-staged config

--- lint-staged.config.cjs
+++ lint-staged.config.cjs
@@ -1,5 +1,5 @@
 // lint-staged.config.cjs
 module.exports = {
-	'src/**/*.{js,jsx,ts,tsx}': ['eslint --cache --fix --max-warnings=0'],
-	'src/**/*.{js,jsx,ts,tsx,css,scss,md,json}': 'prettier --write --ignore-unknown',
+	'src/**/*.{js,jsx,ts,tsx}': ['eslint --cache --fix --max-warnings=0', 'prettier --write --ignore-unknown'],
+	'src/**/*.{css,scss,md,json}': 'prettier --write --ignore-unknown',
 };
